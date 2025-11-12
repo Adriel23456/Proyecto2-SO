d95fda0c0ebf8a8545358962b1d42941b58bbb3a
adriel23456 <adriel.chaves@hotmail.com>
2025-11-12 04:50:38 -0600

    Simple adicion!

--- MainSystem/Master/main.c
+++ MainSystem/Master/main.c
@@ -52,6 +52,9 @@ int main(int argc, char** argv) {
     // ========================================================================
     
     MPI_Init(&argc, &argv);
+    char pname[MPI_MAX_PROCESSOR_NAME]; int plen=0;
+    MPI_Get_processor_name(pname, &plen);
+    printf("[MASTER] Ejecutando en host %s (rank %d)\n", pname, world_rank);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     
